7ab54393e91ad04edb5026c2a1255477f8f84d6a
Farkhod Abdukodirov <abdukodirovfarkhod@gmail.com>
2023-04-23 17:32:10 +0900

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,36 @@
+package org.example;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Test {
+    public static void main(String[] args) {
+
+        String file = "C://Users//FAbdu//Downloads//testData.csv";
+        BufferedReader reader = null;
+        String line = "";
+
+        try{
+            reader = new BufferedReader(new FileReader(file));
+            while((line = reader.readLine())!=null){
+                String[] row = line.split(",");
+                for(String index : row){
+                    System.out.printf("%-20s", index);
+                }
+                System.out.println();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }finally{
+            try {
+                reader.close();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+}
+
+
+
